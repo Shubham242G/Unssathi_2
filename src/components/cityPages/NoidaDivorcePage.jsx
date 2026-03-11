be2b78d62fa53de0e6675df91acbacdffe519ebe
Shubham242G <allhailvicky242@@gmail.com>
2026-03-11 11:54:54 +0530

    meta title changes

--- src/components/cityPages/NoidaDivorcePage.jsx
+++ src/components/cityPages/NoidaDivorcePage.jsx
@@ -1,5 +1,6 @@
 // components/NoidaDivorcePage.jsx
 import { motion } from 'framer-motion';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 const NoidaDivorcePage = () => {
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,11 +6,17 @@ import UnsaathiSolutions from './Solutions';
 import NewBeginning from './NewBegining';
 import HassleFreeProcess from './HassleFreeProcess';
 import ConnectWithUnsaathi from './connectWithUnsaathi';
+import { Helmet } from 'react-helmet-async';
 
 
 function Home() {
   return (
     <div className="min-h-screen bg-[#f5f1ed]">
+      <Helmet>
+        <title>GSLO Unsaathi - Best Divorce Lawyers Delhi NCR</title>
+        <meta name="description" content="Expert divorce lawyers in Delhi, Noida, Gurgaon. Mutual & contested divorces." />
+      </Helmet>
+
       <Hero />
       <HassleFreeProcess/>
       <AboutUs/>
