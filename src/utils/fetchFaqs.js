c5d55dab35b277e3d7829a597fee0db17b3a9c99
Shubham242G <allhailvicky242@@gmail.com>
2026-03-25 14:31:27 +0530

    Service PAge banner and city pages changed

--- src/utils/fetchFaqs.js
+++ src/utils/fetchFaqs.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://unsaathi-backend.onrender.com";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://unsaathi-backend.onrender.com";
 
 export const fetchFaqsByCategory = async (category) => {
   const res = await fetch(`${BASE_URL}/api/faq/by-category/${category}`);
